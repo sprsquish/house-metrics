04a205a17971b88c128c20a189133e7fdc0fad1e
Jeff Smick <jeff@squishtech.com>
2018-06-15 00:03:13 -0700

    fix ambient weather

--- src/main/scala/AmbientWeather.scala
+++ src/main/scala/AmbientWeather.scala
@@ -3,40 +3,52 @@ package smick
 import com.twitter.conversions.time._
 import com.twitter.finagle.Http
 import com.twitter.finagle.http.RequestBuilder
-import com.twitter.util.Future
+import com.twitter.util.{ Duration, Future }
 import java.net.URL
 
 trait AmbientWeather { self: SmickHome =>
   val ambientWeatherMAC = flag("ambientWeather.mac", "", "Weather station MAC")
   val ambientWeatherAPI = flag("ambientWeather.api", "", "Ambient Weather API Key")
   val ambientWeatherApp = flag("ambientWeather.app", "", "Ambient Weather App Key")
-  val ambientWeatherFreq = flag("ambientWeather.freq", 2.seconds, "Ambient Weather polling frequency")
+  val ambientWeatherFreq = flag("ambientWeather.freq", 1.minute, "Ambient Weather polling frequency")
 
   def ambientWeatherLoop(store: Store): Future[Unit] =
     loopIt("ambientWeather", ambientWeatherFreq(), process(store))
 
   private[this] val url = Lazy[URL] {
-    new URL(s"https://api.ambientweather.net/v1/devices/${ambientWeatherMAC()}?apiKey=${ambientWeatherAPI()}&applicationKey=${ambientWeatherApp()}")
+    val u = new URL(s"https://api.ambientweather.net/v1/devices/${ambientWeatherMAC()}?apiKey=${ambientWeatherAPI()}&applicationKey=${ambientWeatherApp()}&limit=1")
+      println(u)
+      u
   }
 
   private[this] val client = Lazy[HttpSvc] {
-    Http.newClient(destStr(url())).toService
+    val _url = url()
+    Http.client
+      .withTls(_url.getHost)
+      .newClient(destStr(_url))
+      .toService
   }
 
   private def process(store: Store): Future[Unit] =
     client()(RequestBuilder().url(url()).buildGet()) flatMap { res =>
       val recs = json.readValue[List[Map[String, Any]]](res.contentString)
       val entries = recs flatMap { data =>
-        StoreEntry("indoor_temp", data("tempinf")) ::
-        StoreEntry("indoor_hum", data("humidityin")) ::
-        StoreEntry("indoor_press", data("baromabsin")) ::
-        StoreEntry("outdoor_temp", data("tempf")) ::
-        StoreEntry("outdoor_hum", data("humidity")) ::
-        StoreEntry("wind_dir", data("winddir")) ::
-        StoreEntry("wind_gust", data("windgustmph")) ::
-        StoreEntry("wind_speed", data("windspeedmph")) ::
-        StoreEntry("solar_radiation", data("solarradiation")) ::
-        StoreEntry("uv_index", data("uv")) ::
+        val ts = {
+          val raw = data("dateutc").asInstanceOf[Long]
+          val d = Duration.fromMilliseconds(raw)
+          Some(d.inNanoseconds)
+        }
+
+        StoreEntry("indoor_temp", data("tempinf"), time = ts) ::
+        StoreEntry("indoor_hum", data("humidityin"), time = ts) ::
+        StoreEntry("indoor_press", data("baromabsin"), time = ts) ::
+        StoreEntry("outdoor_temp", data("tempf"), time = ts) ::
+        StoreEntry("outdoor_hum", data("humidity"), time = ts) ::
+        StoreEntry("wind_dir", data("winddir"), time = ts) ::
+        StoreEntry("wind_gust", data("windgustmph"), time = ts) ::
+        StoreEntry("wind_speed", data("windspeedmph"), time = ts) ::
+        StoreEntry("solar_radiation", data("solarradiation"), time = ts) ::
+        StoreEntry("uv_index", data("uv"), time = ts) ::
         Nil
       }
 
